16ccc7479ba3069545c2f6e2b83c9ac0e30c7b51
Crystal Martin <drcrystalm@gmail.com>
2019-12-06 15:32:24 -0600

    I have 2 tests which is the same amount of brain cells I have after this sprint

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,16 +1,13 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { render } from "@testing-library/react"
 import App from "./App"
+import Header from "./Header"
 
-it("renders without crashing", () => {
-    const div = document.createElement("div")
-    ReactDOM.render(<App />, div)
-    ReactDOM.unmountComponentAtNode(div)
+test("renders without crashing", () => {
+    const container = render(<App />)
 })
 
-describe("App", () => {
-    it("mounts to a simulated DOM", () => {
-        const simulatedDOM = ReactDOM.render(<App />)
-        console.log(simulatedDOM.debug())
-    })
+test("it displays a button with text Push Me", () => {
+    const header = render(<Header />)
+    header.getByText("Push me")
 })
